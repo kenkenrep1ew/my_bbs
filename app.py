46859df477e05fcba4a24bd7d4ccb09f4bdadcf8
Kensaku <Kensaku@hatanakakensaku-no-MacBook-Pro.local>
2021-02-21 15:28:28 +0900

    done file read and write by json

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, url_for, redirect, request
-import sys, os
+import sys, os, time, datetime
+import json
 
 app = Flask(__name__)
 
-DATAFILE = "./data/msg.txt"
+DATAFILE = "./data/msg.json"
+
+# json_card = { "usr":"", "time":"", "msg": ""}
 
 @app.route('/login')
 def login():
@@ -15,21 +18,28 @@ def try_login():
 
 @app.route('/')
 def index():
-	msg = ""
+	json_card = []
 	if os.path.exists(DATAFILE):
-		with open(DATAFILE, 'rt') as f:
-			msg = f.read()
+		with open(DATAFILE, 'r') as f:
+			json_card = json.load(f)
 
 	# app.logger.debug("index")
 	# app.logger.debug(msg)
-	return render_template('index.html', msg=msg)
+	return render_template('index.html', cards=json_card)
 
 @app.route('/write', methods=['POST'])
 def write():
+	json_card = []
+	if os.path.exists(DATAFILE):
+		with open(DATAFILE, 'r') as f:
+			json_card = json.load(f)
 	if 'msg' in request.form:
 		msg = str(request.form["msg"])
-		with open(DATAFILE, 'a') as f:
-			f.write(msg)
+		t = str(datetime.datetime.fromtimestamp(time.time()))
+		u = ""
+		json_card.append({"msg":msg, "time":t, "usr":u})
+		with open(DATAFILE, 'w') as f:
+			json.dump(json_card, f)
 	# app.logger.debug("write")
 	# app.logger.debug(msg)
 	return redirect('/')
