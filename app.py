8f81aee468e7f33e4068b989fbaaeff018b89251
Kensaku <Kensaku@hatanakakensaku-no-MacBook-Pro.local>
2021-02-22 11:15:10 +0900

    add login by session

--- app.py
+++ app.py
@@ -1,23 +1,52 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, session
 import sys, os, time, datetime
 import json
 
 app = Flask(__name__)
+app.secret_key = '087fahvaln'
 
 DATAFILE = "./data/msg.json"
 
-# json_card = { "usr":"", "time":"", "msg": ""}
+USR = {"ken":"aaa", "mai":"bbb",}
+
+# json_card = [{ "usr":"", "time":"", "msg": ""}]
 
 @app.route('/login')
 def login():
 	return render_template('login.html')
 
-@app.route('/try_login')
+@app.route('/try_login', methods=['POST'])
 def try_login():
-	return redirect(url_for('index'))
+	if "name" in request.form:
+		name = request.form["name"]
+	if "pw" in request.form:
+		pw = request.form["pw"]
+	if (name == None) or (pw == None):
+		return redirect('/login')
+	if check_login(name, pw) == False:
+		return redirect('/login')
+	else:
+		return redirect(url_for('index'))
+
+def check_login(name, pw):
+	if not name in USR:
+		return False
+	if USR[name] != pw:
+		return False
+	else:
+		session['login'] = name
+		return True
+
+def is_login():
+	if 'login' in session:
+		return True
+	else:
+		return False
 
 @app.route('/')
 def index():
+	if not is_login():
+		return redirect('/login')
 	json_card = []
 	if os.path.exists(DATAFILE):
 		with open(DATAFILE, 'r') as f:
